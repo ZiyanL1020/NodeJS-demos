38a34eda1c3542f3b89f3011d47c31476ea88305
Ziyan Liu <Ziyan.Liu@genesys.com>
2018-01-03 14:13:54 -0500

    update

--- demo_eventemitter.js
+++ demo_eventemitter.js
@@ -1,13 +1,16 @@
-var http = require('http');
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
-http.createServer(function(req,res){
-    eventEmitter.on('scream', function(){
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end("Scream event is fired");
-    });
-    eventEmitter.emit('scream');
-}).listen(8080);
+eventEmitter.on('connection', function(){
+    console.log("Connect successfully");
+    eventEmitter.emit('data_received');
+});
+
+
+eventEmitter.on('data_received', function(){
+    console.log('data received successfully');
+});
+
+eventEmitter.emit('connection');
+
+console.log("Program ended");
